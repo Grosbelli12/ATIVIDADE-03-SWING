bb6599a1bccfbef37943e34d7f5cb56fc2d35628
GustavoGrosbelli <gustavogrosbelli26@gmail.com>
2024-11-13 13:28:15 -0300

    Pequena continuidade no código

--- src/Aplicativo_de_Notas.java
+++ src/Aplicativo_de_Notas.java
@@ -3,39 +3,43 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Aplicativo_de_Notas {
-    private JPanel lblAplicativoDeNotas;
-    private JTextField txInserirNotas;
+    private JPanel AplicativoDeNotas;
+    private JTextField txtInserirNotas;
     private JButton btnadicionarNota;
-    private JTextArea textArea1;
     private JButton btncalcular;
     private JPanel lblstatus;
+    private JTextArea txtaListadeNotas;
+
+    double nota;
 
     public Aplicativo_de_Notas() {
         btnadicionarNota.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double nota = Double.parseDouble(txInserirNotas.getText());
+                 nota = Double.parseDouble(txtInserirNotas.getText());
             }
         });
         btncalcular.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                double nota = Double.parseDouble(txInserirNotas.getText());
+
             }
         });
+
 
 
     }
     public static void main(String[] args) {
         JFrame frame = new JFrame("Aplicativo de Notas");
-        frame.setContentPane(new Aplicativo_de_Notas().lblstatus);
+        frame.setContentPane(new Aplicativo_de_Notas().AplicativoDeNotas);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
 
 
-
     }
 }
+
+
 
 
